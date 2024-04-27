2a7f909c0d77605ca36dd4c422a460e67792cfb2
Eric Lee <erhlee.bird@gmail.com>
2024-04-27 12:10:36 -0400

    fix: typo in install.sh heredoc

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -3,13 +3,15 @@
 ~/.vim/install.sh
 
 mkdir -p ~/.local/opt/
-( cd ~/.local/opt/ && git clone https://github.com/erhlee-bird/jmp && ./jmp/scripts/install.sh -p ~/.local -b ~/.bashrc )
+( cd ~/.local/opt/ && \
+  git clone https://github.com/erhlee-bird/jmp && \
+  cd jmp && \
+  ./scripts/install.sh -p ~/.local -b ~/.bashrc )
 
 cat >> ~/.bashrc <<EOF
-source ~/.local/opt/jmp/jmp
 alias j=jmp
 
 for config in ~/.dotfiles/bashrc.d/*; do
-  . "${config}"
+  . "\${config}"
 done
 EOF
